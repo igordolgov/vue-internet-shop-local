ce27f358b359db66d09f7c7c09114c4af3ba50b8
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2021-08-06 13:44:46 +0300

    added src directory

---
+++ src/components/CartIndicator.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <router-link> — компонент предназначенный для навигации пользователя в приложении
+  с клиентской маршрутизацией. Путь назначения указывается входным параметром to -->
+  <router-link
+    class="header__cart"
+    aria-label="Корзина с товарами"
+    :to="{name: 'cart'}"
+  >
+    <svg
+      width="30"
+      height="21"
+      fill="currentColor"
+    >
+      <use xlink:href="#icon-cart" />
+    </svg>
+    <span
+      class="header__count"
+      aria-label="Количество товаров"
+    >
+      <!-- C помощью $store получаем доступ к хранилищу (в store/index.js) -->
+      {{ $store.state.cartProducts.length }}
+    </span>
+  </router-link>
+</template>
+
+<script>
+export default {
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'; // Импортируем Vue (из папки node_modules)
+import App from './App.vue'; // Импортируем приложение App.vue (с именем App)
+import router from './router'; // Добавляем router к корневому экземпляру Vue
+import store from './store'; // Импортируем хранилище
+
+Vue.config.productionTip = false; // Отключаем предупреждение о сборках для разработки
+
+// Инициализируем Vue instance и крепим его к div c id "App" (в public/index.html)
+new Vue({
+  router, // Подключаем маршрутизатор (роутер)
+  store, // Подключаем хранилище
+  render: (h) => h(App),
+}).$mount('#app');
