0aa25737b7a8d814c9fbf087b67a491b20305487
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2021-08-12 13:54:47 +0300

    Delete src directory

--- src/helpers/numberFormat.js
+++
@@ -1,6 +0,0 @@
-// Вспомогательная функция форматирования чисел (по разрядам).
-// Функция Intl (из стандартной библиотеки) форматирует число
-// согласно нашей текущей языковой локали
-export default function numberFormat(value) {
-  return new Intl.NumberFormat().format(value);
-}
